571374c41bd7fca8a9a733e4d4f7dca42a21ee28
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2021-04-21 11:52:56 -0300

    tabela hash

---
+++ ativ/aula5hash/aluno.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Aluno{
+  private:
+    int ra;
+    string nome;
+  public:
+    Aluno();
+    Aluno(int r, string n);
+    int obterRa();
+    string obterNome();
+};
